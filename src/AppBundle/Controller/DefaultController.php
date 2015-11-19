27629021a0c3792fb8154d1c5afaed7d16766111
gabriel.pereira <gabriel.pereira@rentcars.com>
2015-11-19 17:20:04 -0200

    teste com deploy

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,7 +13,7 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        var_dump()
+        var_dump('teste');
 
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
